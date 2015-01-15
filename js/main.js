61a92598e327881fabf735fd607f3c2714b27bc9
Sergey <memonanofsa@gmail.com>
2015-01-15 17:56:36 +0300

    scripts1

--- js/main.js
+++ js/main.js
@@ -1,12 +1,3 @@
-$(document).ready(function(){
-	$('.main_slider').slick({
-		dots: true,
-		infinite: true,
-		arrows: true
-	});
-	initialize();
-});
-
 function initialize() {
     var mapCanvas = document.getElementById('map-canvas');
     var mapOptions = {
@@ -16,3 +7,43 @@ function initialize() {
     }
     var map = new google.maps.Map(mapCanvas, mapOptions);
 }
+
+
+
+
+
+$(document).ready(function(){
+	var w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+	var h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+
+	$('.main_slider').height(h-150);
+
+
+
+	$('.main_slider').slick({
+		dots: true,
+		infinite: true,
+		arrows: true
+	});
+	// initialize();
+});
+
+
+
+
+
+window.onscroll = function() {
+	var scrolled = window.pageYOffset || document.documentElement.scrollTop;
+	if(scrolled>= 100){
+		$('.main_menu').css({
+			position:'fixed',
+			top: 0,
+		});
+	}
+	else{
+		$('.main_menu').css({
+			position: 'absolute',
+			top: '100px'
+		});
+	}
+};
